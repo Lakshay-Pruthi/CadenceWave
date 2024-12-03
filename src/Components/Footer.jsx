bbd53e4985e29239bb78aa9f94db21260fd2ce54
Lakshay Pruthi <lakshay47pruthi16@gmail.com>
2024-12-03 19:14:28 +0530

    typo fixed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ import navArrowRight from '../assets/navArrowRight.png'
 function Footer() {
     return (
         <>
-            <div style={{ backgroundColor: 'var(--primary)', color: 'white', borderBottom: '2px solid white', paddingLeft: '5px' }}>Note : This app is supported by Spotify api and it is under going some changes so some functionalities might not be available or you me be redirected for those functionalities.</div>
+            <div style={{ backgroundColor: 'var(--primary)', color: 'white', borderBottom: '2px solid white', paddingLeft: '5px' }}>Note : This app is supported by Spotify api and it is under going some changes so some functionalities might not be available or you may be redirected for those functionalities.</div>
 
             <footer>
                 <button className="windowNavBtn" onClick={() => { window.history.back() }}> <img src={navArrowLeft} alt="" /> </button>
